d46b2361aec9fa64d84089868b2aa2b144a80c53
Esther Pong <e91096@gmail.com>
2021-12-14 17:38:39 -0500

    cognito

--- cognito.tf
+++ cognito.tf
@@ -1,13 +1,13 @@
-# resource "aws_cognito_user_pool" "pool" {
-#   name = "z3l_pool"
-# }
-#
-# resource "aws_cognito_user_pool_client" "client" {
-#   name         = "trucks"
-#   user_pool_id = aws_cognito_user_pool.pool.id
-#   explicit_auth_flows = [
-#     "ALLOW_USER_PASSWORD_AUTH",
-#     "ALLOW_USER_SRP_AUTH",
-#     "ALLOW_REFRESH_TOKEN_AUTH"
-#   ]
-# }
+resource "aws_cognito_user_pool" "pool" {
+  name = "z3l_pool"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "trucks"
+  user_pool_id = aws_cognito_user_pool.pool.id
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+}
