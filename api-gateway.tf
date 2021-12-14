9d7090118c18371d8525ab0c71b40997af93f0f7
Esther Pong <e91096@gmail.com>
2021-12-14 16:17:48 -0500

    cors

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,6 +1,9 @@
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
